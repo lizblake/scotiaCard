12cdebd5bb5092184aac39262e8695b77a0ad3b8
Liz <elizabethblake@psu.edu>
2023-02-06 13:46:18 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -13,15 +13,6 @@ export default {
     ScotiaCardItem,
   },
 };
-
-document.querySelector(".details").addEventListener("click", function (e) {
-  var pTag = document.querySelector("p");
-  if(pTag.style.display === "none") {
-      pTag.style.display = "block";
-  } else {
-    pTag.style.display = "none";
-  }
- }); 
 </script>
 
 <style>
